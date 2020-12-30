884c74e5401db1842d463a9cea378e769328799a
kensuke goto <kensuke.goto.1981@gmail.com>
2020-12-30 20:58:52 +0900

    追加：efsからs3へファイルをコピーする

--- .aws-sam/build/CopyEFSFunction/app.js
+++ .aws-sam/build/CopyEFSFunction/app.js
@@ -1,6 +1,8 @@
+const AWS = require("aws-sdk");
+const S3 = new AWS.S3();
 const fs = require('fs');
 
-const dirPath = '/mnt/efs';
+const dirPath = '/mnt/efs/nuxt';
 // const dirPath = '.';
 exports.lambdaHandler = async (event, context,callback) => {
 
@@ -11,7 +13,7 @@ exports.lambdaHandler = async (event, context,callback) => {
     fs.readdirSync(dir,{ withFileTypes:true })
       .forEach( f => {
         switch(true){
-          case f.isFile(): 
+          case f.isFile():
             list.push(`${dir}/${f.name}`)
             break;
           case f.isDirectory():
@@ -27,8 +29,18 @@ exports.lambdaHandler = async (event, context,callback) => {
   
   const files = list_up_files(dirPath);
   files.forEach(f => {
-    console.log(f);
-  })
+    const f__key = f.replace('/mnt/efs/nuxt/','');
+    // const f__key = f.replace(/^\.\//,'');
+    const f__body = fs.readFileSync(f);
+    S3.putObject({
+      Bucket: 'data-sync-1230',
+      Key: f__key,
+      Body: f__body
+    }, function(err, data) {
+      if (err) console.log(err, err.stack);
+      else console.log(data);
+    });
+  });
 
   callback(null,{
     'statusCode': 200,
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # 参考
 
+- [S3がVPCのプライベートサブネットからアクセス可能になりました！](https://dev.classmethod.jp/articles/vpc-endpoint-for-s3/)
 - [指定したパス配下（サブディレクトリ含む）のファイル一覧を取得](https://kuroeveryday.blogspot.com/2018/03/recursive-directory-search-with-nodejs.html)
 - [ファイルかディレクトリかを調べる](https://nodejs.org/api/fs.html#fs_file_system)
